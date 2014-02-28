fa451e2abadee1f48a6c6673d5a4a8c1ecfbac19
Fredrick Brennan:w <admin@8chan.co>
2014-02-27 20:15:18 -0500

    Refactor

--- mturkcore.py
+++ mturkcore.py
@@ -8,7 +8,6 @@ import requests
 import logging
 import xmltodict
 import collections
-import re
 from datetime import datetime
 
 #Convenient flags for qualification types.
@@ -22,7 +21,6 @@ LOCALE = "00000000000000000071"
 ADULT = "00000000000000000060"
 
 class MechanicalTurk(object):
-	"""The main class. Initialize this class with a valid Python dictionary passed as a string containing properties that mTurk needs to carry out your request. These are use_sandbox, stdout_log [useful to see the request and reply], AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY."""
 	def __init__(self,mturk_config_dict=None):
 		"""Try to set config variables with a file called 'mturkconfig.json' if no argument is passed to the class instance. Else get our config from the argument passed."""
 		if mturk_config_dict is None:
@@ -58,15 +56,7 @@ class MechanicalTurk(object):
 				rv.update({("{}.{}" if inner_key else "{}{}").format(key, inner_key): inner_value})
 		return rv
 
-	def _find_item(self, obj, key):
-		if key in obj: return obj[key]
-		for k, v in obj.items():
-			if isinstance(v, dict):
-				item = self._find_item(v, key)
-				if item is not None:
-					return item
-
-	def create_request(self, operation, request_parameters={}):
+	def request(self, operation, request_parameters={}):
 		"""Create a Mechanical Turk client request. Unlike other libraries (thankfully), my help ends here. You can pass the operation (view the list here: http://docs.amazonwebservices.com/AWSMechTurk/latest/AWSMturkAPI/ApiReference_OperationsArticle.html) as parameter one, and a dictionary of arguments as parameter two. To send multiple of the same argument (for instance, multiple workers to notify in NotifyWorkers), you can send a list."""
 		self.operation = operation
 
@@ -78,24 +68,32 @@ class MechanicalTurk(object):
 		timestamp = self._generate_timestamp(time.gmtime())
 		signature = self._generate_signature(operation, timestamp, self.aws_secret_key)
 
-	# Add common parameters to request dict
+		# Add common parameters to request dict
 		request_parameters.update({"Operation":operation,"Version":"2012-03-25","AWSAccessKeyId":self.aws_key,"Signature":signature,"Timestamp":timestamp})
 
 		self.flattened_parameters = self._flatten(request_parameters)
 
 		request = requests.post(self.service_url, params=self.flattened_parameters)
 		request.encoding = 'utf-8'
-		self.xml_response = request.text # Store XML response, might need it
-		self.response = xmltodict.parse(self.xml_response.encode('utf-8'), dict_constructor=dict)
-		return self.response
+		xml = request.text # Store XML response, might need it
+		response = xmltodict.parse(xml.encode('utf-8'), dict_constructor=dict)
+		return MechanicalTurkResponse(response, xml=xml)
 
-	def get_response_element(self, element, response=None):
-		if response is None:
-			response = self.response
-		return self._find_item(response, element)	
+class MechanicalTurkResponse(dict):
+	def __init__(self, response, xml=None):
+		dict.__init__(self, response)
+		self.response = response
+		self.xml = xml
+		req = self.get_response_element("Request")
+		self.valid = req.get("IsValid") == "True" if req else False
 
-	def is_valid(self, response=None):
-		"""Convenience function to figure out if the last request we made was valid."""
-		if response is None:
-			response = self.response
-		return self.get_response_element("Request", response=response)["IsValid"] == "True"
+	def _find_item(self, obj, key):
+		if key in obj: return obj[key]
+		for k, v in obj.items():
+			if isinstance(v, dict):
+				item = self._find_item(v, key)
+				if item is not None:
+					return item
+
+	def get_response_element(self, element):
+		return self._find_item(self.response, element)	
